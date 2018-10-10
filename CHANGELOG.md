a370f74aeffccf02b2fab3b244a44aa7f0987234
Ilya Reshetnikov <devxom@gmail.com>
2018-10-10 15:06:33 +0300

    Realise first version babel config
    tag: v0.0.1
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+<a name="0.0.1"></a>
+## [0.0.1](https://github.com/MyBook/selfpub-ui/compare/v1.7.6...v0.0.1) (2018-10-10)
+
+### Feature
+
+* Realise first version babel config
---
+++ index.js
@@ -0,0 +1,50 @@
+/* global require, module, process */
+"use strict";
+
+const modernTargets = {
+  browsers: [
+    "last 2 Chrome versions",
+    "not Chrome < 60",
+    "last 2 Safari versions",
+    "not Safari < 10.1",
+    "last 2 iOS versions",
+    "not iOS < 10.3",
+    "last 2 Firefox versions",
+    "not Firefox < 54",
+    "last 2 Edge versions",
+    "not Edge < 15",
+  ],
+};
+
+module.exports = function(context, options) {
+  const {
+    modules = false,
+    useBuiltIns = false,
+    targets = modernTargets,
+  } = options;
+
+  const development =
+    typeof options.development === "boolean" ? options.development : false;
+
+  return {
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          modules,
+          useBuiltIns,
+          targets,
+        },
+      ],
+      [require("@babel/preset-react"), { development }],
+    ],
+    plugins: [
+      require("@babel/plugin-transform-modules-commonjs"),
+      require("@babel/plugin-proposal-class-properties"),
+      require("@babel/plugin-proposal-export-default-from"),
+      require("@babel/plugin-proposal-function-bind"),
+      require("@babel/plugin-proposal-throw-expressions"),
+      require("@babel/plugin-syntax-dynamic-import"),
+    ].filter(Boolean),
+  };
+};
