4d108d6a5390ff6ae6564ecc4c11d2faf397e50c
eshavrov <shavrov.ep@gmail.com>
2020-01-21 11:59:38 +0300

    Fixed plugins config

--- external-plugins.js
+++ external-plugins.js
@@ -2,23 +2,23 @@ module.exports = function externalPlugins(options = {}) {
   const { styledComponents, removePropTypes } = options;
 
   const styledComponentsEnable = !!styledComponents;
-  const styledComponentsOptions =
-    typeof styledComponents === "object" ? styledComponents : {};
-
   const removePropTypesEnable = !!removePropTypes;
-  const removePropTypesOptions =
-    typeof removePropTypes === "object" ? removePropTypes : {};
 
-  return [
+  const plugins = [
     require.resolve("@babel/plugin-transform-flow-strip-types"),
     require.resolve("babel-plugin-dynamic-import-node"),
     require.resolve("@babel/plugin-proposal-class-properties"),
     require.resolve("@babel/plugin-proposal-function-bind"),
     require.resolve("@babel/plugin-syntax-dynamic-import"),
     require.resolve("@babel/plugin-proposal-nullish-coalescing-operator"),
-    require.resolve("@babel/plugin-proposal-optional-chaining"),
+    require.resolve("@babel/plugin-proposal-optional-chaining")
+  ];
 
-    styledComponentsEnable && [
+  if (styledComponentsEnable) {
+    const styledComponentsOptions =
+      typeof styledComponents === "object" ? styledComponents : {};
+
+    plugins.push([
       "babel-plugin-styled-components",
       Object.assign(
         {
@@ -28,10 +28,15 @@ module.exports = function externalPlugins(options = {}) {
         },
         styledComponentsOptions
       )
-    ],
+    ]);
+  }
 
-    removePropTypesEnable && [
-      require("babel-plugin-transform-react-remove-prop-types"),
+  if (removePropTypesEnable) {
+    const removePropTypesOptions =
+      typeof removePropTypes === "object" ? removePropTypes : {};
+
+    plugins.push([
+      "babel-plugin-transform-react-remove-prop-types",
       Object.assign(
         {
           mode: "wrap",
@@ -40,6 +45,8 @@ module.exports = function externalPlugins(options = {}) {
         },
         removePropTypesOptions
       )
-    ]
-  ];
+    ]);
+  }
+
+  return plugins;
 };
